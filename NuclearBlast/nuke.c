fae4b24c3935ae5804dd01811aabe4426a796070
bbelandr <bbelandr@vols.utk.edu>
2023-09-06 06:01:11 -0400

    working input parsing and sieverts calculation

--- NuclearBlast/nuke.c
+++ NuclearBlast/nuke.c
@@ -1,7 +1,21 @@
 // Benjamin Belandres
+// This program simulates a nuclear blast from certain coordinates taken in from the command line
+    // Using this program
+        // The command is formatted like ./nuke <blast strength> <attenuation> <blast x> <blast y>
+        // AFterwards, the program begins reading in people at certain coordinates
+            // <x> <y> <name>
+        // The program then immediately calculates how many sieverts that person would recieve and prints it
+        // This continues until the program no longer has names to read
+            // This strategy avoids having to store all the people into memory, and we can just work with
+            // what immediately is given to the program
+    // The sieverts calculation
+        // sieverts = blastStrength * (attenuation)^distance 
+        // Distance
+            // sqrt((x1 - x0)^2 + (y1 - y0)^2)
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 struct Point {  // Holds an x and y coordinate on the grid
     int x;
@@ -9,7 +23,7 @@ struct Point {  // Holds an x and y coordinate on the grid
 };
 
 struct Person {
-    struct Point location;
+    struct Point* location;
     char name[65];  // Takes a maximum of 64 characters plus the null character at the end
 };
 
@@ -23,24 +37,39 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    double blastStrength = strtod(argv[1]);
-    double attenuation = strtod(argv[2]);
-    int blastX = strtoi(argv[3]);
-    int blastY = strtoi(argv[4]);
+    // Read in bomb
+    char* end;  // The position of the last character + 1 read in by strtod is stored in this
+    double blastStrength = strtod(argv[1], &end);
+    double attenuation = strtod(argv[2], &end);
+    
+    struct Point* blastLocation = malloc(sizeof(struct Point));
+    blastLocation->x = (int)strtol(argv[3], &end, 10);
+    blastLocation->y = (int)strtol(argv[4], &end, 10);
+
+    // Read in person
+    struct Person currentPerson;
+    currentPerson.location = malloc(sizeof(struct Point));
+    scanf("%d %d %64s", &currentPerson.location->x, &currentPerson.location->y, currentPerson.name);
+    // currentPerson.location->x = 1;
+    // currentPerson.location->y = 5;
 
 
+    // Calculations
+    double sieverts = pow(attenuation, distance(blastLocation, currentPerson.location)) * blastStrength;
+
+    printf("%-16.16s:%9.4lf Sv.\n", currentPerson.name, sieverts);
 
     
-    struct Person currentPerson;
     // while Read in a person formatted like:
         // x y name
 
 
+    free(blastLocation);
+    free(currentPerson.location);
     return 0;
 }
 
 static double distance(const struct Point *p1, const struct Point *p2) {
     
-    
-    return -1;
+    return sqrt( pow(p2->x - p1->x, 2) + pow(p2->y - p1->y, 2));
 }
