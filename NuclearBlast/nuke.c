735dbefa8fed67118d9e2e0a572c78f68c1787a2
Benja <bbelandr@vols.utk.edu>
2023-09-06 06:52:22 -0400

    working version, correct output and errors

--- NuclearBlast/nuke.c
+++ NuclearBlast/nuke.c
@@ -14,8 +14,8 @@
             // sqrt((x1 - x0)^2 + (y1 - y0)^2)
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
+#include <stdlib.h> // I genuinely have no idea how to convert the command line arguments to numbers without strtod and strtol
 
 struct Point {  // Holds an x and y coordinate on the grid
     int x;
@@ -23,7 +23,7 @@ struct Point {  // Holds an x and y coordinate on the grid
 };
 
 struct Person {
-    struct Point* location;
+    struct Point location;
     char name[65];  // Takes a maximum of 64 characters plus the null character at the end
 };
 
@@ -38,38 +38,49 @@ int main(int argc, char *argv[]) {
     }
 
     // Read in bomb
+        // Everything in the command line is error checked so the user knows what went wrong
     char* end;  // The position of the last character + 1 read in by strtod is stored in this
     double blastStrength = strtod(argv[1], &end);
+    if (end == argv[1]) {   // if end == the beginning of argv[1], then the program managed to read nothing
+        printf("Error: unable to read blast strength.\n");
+        return -1;
+    }
     double attenuation = strtod(argv[2], &end);
+    if (end == argv[2]) {
+        printf("Error: unable to read blast attenuation.\n");
+        return -1;
+    }
     
-    struct Point* blastLocation = malloc(sizeof(struct Point));
-    blastLocation->x = (int)strtol(argv[3], &end, 10);
-    blastLocation->y = (int)strtol(argv[4], &end, 10);
+    struct Point blastLocation;
+    blastLocation.x = (int)strtol(argv[3], &end, 10);
+    if (end == argv[3]) {
+        printf("Error: unable to read blast x coordinate.\n");
+        return -1;
+    }
+    blastLocation.y = (int)strtol(argv[4], &end, 10);
+    if (end == argv[4]) {
+        printf("Error: unable to read blast y coordinate.\n");
+        return -1;
+    }
 
-    // Read in person
+
+    // Read in new person
     struct Person currentPerson;
-    currentPerson.location = malloc(sizeof(struct Point));
-    scanf("%d %d %64s", &currentPerson.location->x, &currentPerson.location->y, currentPerson.name);
-    // currentPerson.location->x = 1;
-    // currentPerson.location->y = 5;
-
-
-    // Calculations
-    double sieverts = pow(attenuation, distance(blastLocation, currentPerson.location)) * blastStrength;
-
-    printf("%-16.16s:%9.4lf Sv.\n", currentPerson.name, sieverts);
-
     
-    // while Read in a person formatted like:
-        // x y name
+    while (scanf("%d %d %64s", &currentPerson.location.x, &currentPerson.location.y, currentPerson.name) == 3) {
+        
+        // Calculations
+        double sieverts = pow(attenuation, distance(&blastLocation, &currentPerson.location)) * blastStrength;
+
+        printf("%-16s: %9.4lf Sv.\n", currentPerson.name, sieverts);
+
+    }
 
 
-    free(blastLocation);
-    free(currentPerson.location);
     return 0;
 }
 
 static double distance(const struct Point *p1, const struct Point *p2) {
-    
+    // DEBUG printf("%d %d %d %d\n", p1->x, p1->y, p2->x, p2->y);
     return sqrt( pow(p2->x - p1->x, 2) + pow(p2->y - p1->y, 2));
 }
