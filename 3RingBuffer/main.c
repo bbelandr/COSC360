1da8600a9b29067fdd8733063d03a2ccc970081a
Benja <bbelandr@vols.utk.edu>
2023-09-20 07:35:38 -0400

    Working version

--- 3RingBuffer/main.c
+++ 3RingBuffer/main.c
@@ -1,10 +1,11 @@
 // Benjamin Belandres
 #include "rbuf.h"
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 
-    RingBuffer *rb = rb_new(3);
+    RingBuffer *rb = rb_new(10);
 
     printf("Capacity:       %3d\n", rb_capacity(rb));
     printf("Size:           %3d\n", rb_size(rb));
@@ -12,32 +13,118 @@ int main() {
     // printf("%d Added 4 to the buffer\n", rb_push(rb, 'a'));
     rb_push(rb, 'a');
     rb_push(rb, 's');
-    rb_push(rb, 's');
-    rb_push(rb, 'r');
+    rb_push(rb, 'k');
+    rb_push(rb, 'e');
     rb_push(rb, 'r');
 
+    printf("\nAfter adding values:\n");
     printf("Capacity:       %3d\n", rb_capacity(rb));
     printf("Size:           %3d\n", rb_size(rb));
     printf("Current index:  %3d\n", rb_at(rb));
 
 
     char val;
-    bool ret;
+    int ret;
 
-    ret = rb_pop(rb, &val);
-    printf("%d Popped (Size %d): %c\n", ret, rb_size(rb), val);
-    ret = rb_pop(rb, &val);
-    printf("%d Popped (Size %d): %c\n", ret, rb_size(rb), val);
-    ret = rb_pop(rb, &val);
-    printf("%d Popped (Size %d): %c\n", ret, rb_size(rb), val);
-    ret = rb_pop(rb, &val);
-    printf("%d Popped (Size %d): %c\n", ret, rb_size(rb), val);
-    ret = rb_pop(rb, &val);
-    printf("%d Popped (Size %d): %c\n", ret, rb_size(rb), val);
-    ret = rb_pop(rb, &val);
-    printf("%d Popped (Size %d): %c\n", ret, rb_size(rb), val);
-    ret = rb_pop(rb, &val);
-    printf("%d Popped (Size %d): %c\n", ret, rb_size(rb), val);
+    // rb_clear(rb);
+    rb_ignore(rb, 2);
+
+    printf("\nAfter ignoring:\n");
+    printf("Capacity:       %3d\n", rb_capacity(rb));
+    printf("Size:           %3d\n", rb_size(rb));
+    printf("Current index:  %3d\n", rb_at(rb));
+
+
+    // // CHECKING WRITE
+    // char inputString[] = "pizaPie";
+    // char outputString[strlen(inputString)];
+    // rb_ignore(rb, 2);
+
+    // printf("\nAfter ignoring vals:\n");
+    // printf("Capacity:       %3d\n", rb_capacity(rb));
+    // printf("Size:           %3d\n", rb_size(rb));
+    // printf("Current index:  %3d\n", rb_at(rb));
+
+
+    // ret = rb_write(rb, inputString, 6);
+    // printf("Successfully wrote %d bytes\n", ret);
+
+    // printf("Successfully read %d bytes\n", rb_read(rb, outputString, 9));
+    // printf("%s\n", outputString);
+
+    // rb_ignore(rb, 3);
+
+    // printf("\nAfter ignoring vals:\n");
+    // printf("Capacity:       %3d\n", rb_capacity(rb));
+    // printf("Size:           %3d\n", rb_size(rb));
+    // printf("Current index:  %3d\n", rb_at(rb));
+
+    // ret = rb_write(rb, inputString, 6);
+    // printf("Successfully wrote %d bytes\n", ret);
+
+    // printf("Successfully read %d bytes\n", rb_read(rb, outputString, 9));
+    // printf("%s\n", outputString);
+
+
+    // // CHECKING READ
+    // char myString[5];
+
+    // rb_ignore(rb, 2);
+    // rb_push(rb, 't');
+    // rb_push(rb, 'i');
+
+    // printf("Successfully read %d bytes\n", rb_read(rb, myString, 9));
+
+    // printf("%s\n", myString);
+    // printf("At is currently looking at: %c\n", rb_peek(rb));
+
+    // printf("\nAfter reading:\n");
+    // printf("Capacity:       %3d\n", rb_capacity(rb));
+    // printf("Size:           %3d\n", rb_size(rb));
+    // printf("Current index:  %3d\n", rb_at(rb));
+
+
+
+    // // CHECKING IGNORE
+    // rb_ignore(rb, 3);
+    // printf("\nIgnored Values\n");
+    // printf("Capacity:       %3d\n", rb_capacity(rb));
+    // printf("Size:           %3d\n", rb_size(rb));
+    // printf("Current index:  %3d\n", rb_at(rb));
+    // printf("At is currently looking at: %c\n", rb_peek(rb));
+
+
+    // // CHECKING POP
+    // bool ret;
+    // ret = rb_pop(rb, &val);
+    // printf("%d Popped (Size %d): %c\n", ret, rb_size(rb), val);
+    // printf("Current index:  %3d\n", rb_at(rb));
+    // ret = rb_pop(rb, &val);
+    // printf("%d Popped (Size %d): %c\n", ret, rb_size(rb), val);
+    // printf("Current index:  %3d\n", rb_at(rb));
+    // ret = rb_pop(rb, &val);
+    // printf("%d Popped (Size %d): %c\n", ret, rb_size(rb), val);
+    // printf("Current index:  %3d\n", rb_at(rb));
+    // ret = rb_pop(rb, &val);
+    // printf("%d Popped (Size %d): %c\n", ret, rb_size(rb), val);
+    // printf("Current index:  %3d\n", rb_at(rb));
+    // ret = rb_pop(rb, &val);
+    // printf("%d Popped (Size %d): %c\n", ret, rb_size(rb), val);
+    // printf("Current index:  %3d\n", rb_at(rb));
+    // ret = rb_pop(rb, &val);
+    // printf("%d Popped (Size %d): %c\n", ret, rb_size(rb), val);
+    // printf("Current index:  %3d\n", rb_at(rb));
+    // ret = rb_pop(rb, &val);
+    // printf("%d Popped (Size %d): %c\n", ret, rb_size(rb), val);
+    // printf("Current index:  %3d\n", rb_at(rb));
+
+    // rb_push(rb, 'p');
+    // printf("\nCurrent index:  %3d\n", rb_at(rb));
+
+    // ret = rb_pop(rb, &val);
+    // printf("%d Popped (Size %d): %c\n", ret, rb_size(rb), val);
+    // printf("Current index:  %3d\n", rb_at(rb));
+
 
     rb_free(rb);
     return 0;
--- 3RingBuffer/rbuf.c
+++ 3RingBuffer/rbuf.c
@@ -2,8 +2,12 @@
 
 #include "rbuf.h"
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>  // For debugging
 
+// Normally a ringbuffer like this would have a head and tail index, but this ringbuffer
+// only uses a tail (int at). 
+// To learn the loaction of the head, I implemented a static head function
 struct RingBuffer
 {
     int at;         // The index of the next byte to read
@@ -59,17 +63,24 @@ int rb_capacity(const struct RingBuffer *rb) {
     return rb->capacity;
 }
 
+
 // Advances the at index to the next value and wraps around
 // the ring if it goes past the end
-static void incrementAt(struct RingBuffer *rb) {
-    rb->at++;
+static void incrementAt(struct RingBuffer *rb, int amount) {
+    rb->at += amount;
     
     // Wrapping the at index around if it has reached capacity
-    if (rb->at > rb->capacity - 1) {
-        rb->at = rb->at % rb->capacity;
-    }
+    // if (rb->at > rb->capacity - 1) {
+    rb->at = rb->at % rb->capacity;
+    // }
     // DEBUG printf("At is currently at index: %d\n", rb->at);
 }
+
+// Returns where the head would be if there were a head variable
+static int head(struct RingBuffer *rb) {
+    return (rb->at + rb->size) % rb->capacity;
+}
+
 
 // Adds data to the RingBuffer where the at pointer is looking
 // Returns true if the data was added successfully, false if not
@@ -77,14 +88,14 @@ bool rb_push(struct RingBuffer *rb, char data) {
     // Check if the capacity can fit the new value
     if (rb->size < rb->capacity) {
         
-        // Place the new value into buffer[at]
-        rb->buffer[rb->at] = data;
+        // Place the new value at the head
+        rb->buffer[head(rb)] = data;
 
-        // printf("Added %c to the buffer at %d\n", rb->buffer[rb->at], rb->at);
-
-        incrementAt(rb);
-        
+        // printf("Added %c to the buffer at %d\n", rb->buffer[rb->size + rb->at], rb->size + rb->at);
         rb->size++;
+
+        // incrementAt(rb);
+        
         return true;
     }
     else {
@@ -107,8 +118,10 @@ bool rb_pop(struct RingBuffer *rb, char *data) {
         }
 
         rb->size--;
+        // rb->at++; 
+        incrementAt(rb, 1);
 
-        incrementAt(rb);
+
 
         return true;
     }
@@ -120,6 +133,98 @@ bool rb_pop(struct RingBuffer *rb, char *data) {
 
 // Returns the value of the next byte
 // Returns 0 if nothing is found
+    // Be aware that this function can also return 0 if it sees '\0' 
+    // on the ring buffer. Checking the size of the buffer before 
+    // using this function is the user's responsibility.
+// This function does NOT change the at index or the size
 char rb_peek(const struct RingBuffer *rb) {
+    
+    if (rb->size > 0) {
+        return rb->buffer[rb->at];
+    }
+    else {
+        return 0;
+    }
+}
 
+
+// Skips num amount of values from the at index
+// This effectively removes num values from the buffer
+// Instead of calling pop over and over again, this function simply
+// changes the size and the at index, saving processing time.
+void rb_ignore(struct RingBuffer *rb, int num) {
+    if (num > rb->size) {
+        num = rb->size;
+    }
+
+    rb->size = rb->size - num;
+
+    incrementAt(rb, num);
+    // rb->at = (rb->at + num) % rb->capacity;
+}
+
+
+// Places information from the ringbuffer to the memory address
+// pointed to by buf. 
+// Decrements the size for every byte read from the ringbuffer and
+// increments the at pointer.
+// Returns the actual number of bytes read and put into buf.
+int rb_read(struct RingBuffer *rb, char *buf, int max_bytes) {
+    
+    if (max_bytes > rb->size) {
+        max_bytes = rb->size;
+    }
+
+    // We're going to have to loop around the buffer
+    if (rb->at + max_bytes > rb->capacity) {
+        // Doing the right side
+        int bytes_read = rb->size - rb->at;
+        memcpy(buf, &rb->buffer[rb->at], bytes_read);
+
+        // Doing the left side
+        memcpy(&buf[bytes_read], rb->buffer, max_bytes - bytes_read);
+    }
+    else {
+        // No looping necessary
+        memcpy(buf, &rb->buffer[rb->at], max_bytes);
+    }
+
+
+    incrementAt(rb, max_bytes);
+    rb->size = rb->size - max_bytes;
+
+    return max_bytes;
+}
+
+
+// Writes as many bytes to the RingBuffer as can fit from buf
+// bytes is the max amount of bytes to write
+// Returns the number of bytes actually written to the buffer
+int rb_write(struct RingBuffer *rb, const char *buf, int bytes) {
+    if (rb->capacity - rb->size < bytes) {
+        bytes = rb->capacity - rb->size;    // The actual number of bytes we're going to be able to write
+    }
+
+    if (head(rb) + bytes > rb->capacity) {
+        // We have to loop
+        // Doing the right side
+        int bytes_written = rb->capacity - head(rb);
+        memcpy(&rb->buffer[head(rb)], buf, bytes_written);
+
+        // Doing the left side
+        memcpy(rb->buffer, &buf[bytes_written], bytes - bytes_written);
+    }
+    else {
+        // No looping necessary
+        memcpy(&rb->buffer[head(rb)], buf, bytes);
+    }
+    
+    rb->size += bytes;
+    return bytes;
+}
+
+
+void rb_clear(struct RingBuffer *rb) {
+    rb->at = 0;
+    rb->size = 0;
 }
