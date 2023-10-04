b488311a4b9d1db6480ed11c7790cca7331737ea
Benja <bbelandr@vols.utk.edu>
2023-10-04 15:01:50 -0400

    Beginning page_init()

--- 5PageAllocator/main.c
+++ 5PageAllocator/main.c
@@ -6,12 +6,25 @@
 #include "pageAlloc.h"
 
 bool page_init(int pages) {
-	(void) pages;
+	const int PAGE_SIZE = 4096;		// How large a single page is in bytes
+	const int MAX_PAGES = 262144;	// The maximum amount of pages allowed on the machine
+
+	// The user must ask for more than 1 page and less than MAX_PAGES
+	if (pages < 2) {
+		return false;
+	}
+	if (pages > MAX_PAGES) {
+		return false;
+	}
+
 	return false;
 }
 
 int main() {
 
 	printf("I like chicken and dizza\n");
+	
+	page_init(1);
+	
 	return 0;
 }
