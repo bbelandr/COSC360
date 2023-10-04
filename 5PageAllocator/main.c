efc1b0cf4e9929c321845a196ee48a2a4702450b
Benja <bbelandr@vols.utk.edu>
2023-10-04 10:42:20 -0400

    adding a makefile

--- 5PageAllocator/main.c
+++ 5PageAllocator/main.c
@@ -5,8 +5,13 @@
 #include <stdio.h>
 #include "pageAlloc.h"
 
-
+bool page_init(int pages) {
+	(void) pages;
+	return false;
+}
 
 int main() {
+
+	printf("I like chicken and dizza\n");
 	return 0;
 }
